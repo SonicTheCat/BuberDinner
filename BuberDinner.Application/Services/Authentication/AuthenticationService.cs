49ce003a9d166efc3f07810eab5c533c35f250ae
SonicTheCat <paveliliev90@gmail.com>
2024-08-16 21:16:25 +0300

    Create JwtToken generator.

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,15 +1,32 @@
 
+using BuberDinner.Application.Common.Interfaces.Authentication;
+
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        this.jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public AuthenticationResult Login(string email, string password)
     {
         return new AuthenticationResult(new Guid(), "firstname", "lastName", email, "token");
     }
-    
+
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        return new AuthenticationResult(new Guid(), firstName, lastName, email, "token");
+        // Check if User already exists
+
+        // Create User (generate unique Id)
+
+        // Generate JWT token 
+        var userId = new Guid();
+        var token = this.jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+        return new AuthenticationResult(userId, firstName, lastName, email, token);
     }
 }
